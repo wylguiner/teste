30843848e291650e233dcf0bce19d051d37ed5b4
Wylguiner <wylguiner0@hotmail.com>
2019-01-11 15:31:34 -0200

    teste

--- conexao.php
+++ conexao.php
@@ -6,10 +6,4 @@ $pass = "";
 $dbname = "suit";
 $conn = mysqli_connect($server,$usuario,$pass,$dbname);
 
-$serverH = "ec2-107-22-238-186.compute-1.amazonaws.com";
-$usuarioH = "uqpboeiaheqpth";
-$passH = '617467c5d3f80d547a4e58aed8f5d936a1406b132311dc4366e401674dde6ba6';
-$dbnameH = "d1smj8212gls1";
-$connH = mysqli_connect($serverH,$usuarioH,$passH,$dbnameH);
-
 ?>
--- testeConexaoHeroku.php
+++ testeConexaoHeroku.php
@@ -1,7 +1,21 @@
 <?php
-include('conexao.php');
 
-mysqli_connect("host", "user", "password", "db") or die(mysqli_error());
+$serverH = "ec2-107-22-238-186.compute-1.amazonaws.com";
+$usuarioH = "uqpboeiaheqpth";
+$passH = '617467c5d3f80d547a4e58aed8f5d936a1406b132311dc4366e401674dde6ba6';
+$dbnameH = "d1smj8212gls1";
 
-echo "Connected to MySQL<br />";
 
+$servername = "ec2-107-22-238-186.compute-1.amazonaws.com";
+$username = "uqpboeiaheqpth";
+$password = "617467c5d3f80d547a4e58aed8f5d936a1406b132311dc4366e401674dde6ba6";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
